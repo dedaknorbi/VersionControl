e6e44c603f1531b626c1362540a4cf4a7c567362
Norbert Dedák <ghoztfacer@gmail.com>
2020-10-04 03:14:21 +0200

    f

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -88,17 +88,34 @@ namespace WindowsFormsApp2
                 values[counter, 1] = f.Vendor;
                 values[counter, 2] = f.Side;
                 values[counter, 3] = f.District;
-                values[counter, 4] = f.Elevator;
+                if (f.Elevator)
+                {
+                    values[counter, 4] = "VAN";
+                }
+                else
+                {
+                    values[counter, 4] = "NINCS";
+                }
                 values[counter, 5] = f.NumberOfRooms;
                 values[counter, 6] = f.FloorArea;
                 values[counter, 7] = f.Price;
-                values[counter, 8] = "";
+                values[counter, 8] = "="+GetCell(counter+2,7)+"/"+ GetCell(counter+2, 6);
                 counter++;
             }
-            GetCell(Flats.Count, headers.Length);
+            
             xlSheet.get_Range(
              GetCell(2, 1),
              GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
         }
         private string GetCell(int x, int y)
         {
