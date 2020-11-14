d42fe2b831d9c6deb6d23f5d41486babe316bfc1
Norbert Dedák <ghoztfacer@gmail.com>
2020-11-14 16:21:19 +0100

    asd

---
+++ 8-adik gyak/Abstractions/IToyFactory.cs
@@ -0,0 +1,14 @@
+﻿using _8_adik_gyak.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_adik_gyak.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
